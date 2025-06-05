c28e1f975498e36f928fceb61bfbf624510a535f
GabrielFideles <gabrielfidelesjgfs@gmail.com>
2025-06-05 15:16:13 -0300

    feat: Otimização de buscas para SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://clinicasallute.com.br',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
+  },
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    },
+    domains: ['clinicasallute.com.br'],
+    remotePatterns: [{ protocol: "https" }]
+  },
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto'
   }
 });
